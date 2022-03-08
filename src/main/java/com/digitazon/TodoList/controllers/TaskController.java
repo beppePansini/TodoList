1a8ca4fdfc3b1be67259c7cf68eec5b27ad46e8c
CeciliaAlbicini <cecilia.albicini@gmail.com>
2022-03-08 13:01:36 +0100

    second commit

---
+++ src/main/java/com/digitazon/TodoList/controllers/TaskController.java
@@ -0,0 +1,22 @@
+package com.digitazon.TodoList.controllers;
+
+import com.digitazon.TodoList.repositories.TaskRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController //fa parsing risultati in json
+public class TaskController {
+
+    private TaskRepository taskRepository;
+
+    public TaskController(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    @GetMapping("/")
+    public String home() {
+        long tot = taskRepository.count();
+        System.out.println(tot);
+        return "Benvenuti!";
+    }
+}
